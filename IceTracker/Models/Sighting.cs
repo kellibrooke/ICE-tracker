80334588c4077a0ea20a9876d9745a6354432aa9
Kelli Brooke and Renee Sarley <renee@>
2018-07-23 11:39:37 -0700

    add sighting and user models with basic methods

--- IceTracker/Models/Sighting.cs
+++ IceTracker/Models/Sighting.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using MySql.Data.MySqlClient;
+using RestSharp;
+using RestSharp.Authenticators;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 
 namespace IceTracker.Models
 {
@@ -37,27 +43,23 @@ namespace IceTracker.Models
         public void Alert()
 
         {
-            // get phone numbers
+            List<User> allUsers = User.GetAllUsers();
 
-            //    //1
-            //    var client = new RestClient("https://api.twilio.com/2010-04-01/Accounts");
+            for (int i = 0; i < allUsers.Count; i++)
+            {
+                const string accountSid = "AC674ab5f2c2d97142070400153c34bcfa";
+                const string authToken = "843f163e0a2dfa22f7348a04cadd7eee";
+                TwilioClient.Init(accountSid, authToken);
 
-            //    //2
-            //    var request = new RestRequest("Accounts/AC674ab5f2c2d97142070400153c34bcfa/Messages", Method.POST);
-            //    //3
-            //    request.AddParameter("To", "+15038631605");
-            //    request.AddParameter("From", "+19718034174");
-            //    request.AddParameter("Body", "Hello world!");
-            //    //4
-            //    client.Authenticator = new HttpBasicAuthenticator("AC674ab5f2c2d97142070400153c34bcfa", "843f163e0a2dfa22f7348a04cadd7eee");
-            //    //5
-            //    client.ExecuteAsync(request, response =>
-            //    {
-            //        Console.WriteLine(response);
-            //    });
-            //    Console.ReadLine();
+                var to = new PhoneNumber("+1" + allUsers[i].PhoneNumber);
+                var message = MessageResource.Create(
+                    to,
+                    from: new PhoneNumber("+19718034174"),
+                    body: this.Description);
 
-            //cycle through numbers and do trillio thing
+                Console.WriteLine(message.Sid);
+            }
+
         }
 
 
---
+++ IceTracker/Models/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace IceTracker.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+        public User(string phoneNumberInput, int idInput = 0)
+        {
+            Id = idInput;
+            PhoneNumber = phoneNumberInput;
+        }
+
+        public static List<User> GetAllUsers()
+        {
+            List<User> allUsers = new List<User> { };
+
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM users;";
+
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while (rdr.Read())
+            {
+                int id = rdr.GetInt32(0);
+                string phoneNumber = rdr.GetString(1);
+ 
+                User newUser = new User(phoneNumber, id);
+                allUsers.Add(newUser);
+            }
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+
+            return allUsers;
+
+        }
+    }
+}
