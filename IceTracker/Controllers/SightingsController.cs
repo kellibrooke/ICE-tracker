a38d2a9d844f3f7ca959491e8fb436368bde88ed
Nikki Boyd <boyd.nikki@icloud.com>
2018-07-24 14:18:09 -0700

    style throughout application

--- IceTracker/Controllers/SightingsController.cs
+++ IceTracker/Controllers/SightingsController.cs
@@ -5,13 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using IceTracker.Models;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace IceTracker.Controllers
 {
     public class SightingsController : Controller
     {
-        // GET: /<controller>/
+        [HttpGet("/sightings")]
         public IActionResult Index()
         {
             return View();
