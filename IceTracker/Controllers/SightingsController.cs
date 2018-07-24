cc7e3c2117ab50ee7c866738871fa0590e68b69d
Kelli <kellibrookelife@gmail.com>
2018-07-24 14:24:46 -0700

    add list of sightings to user account page

--- IceTracker/Controllers/SightingsController.cs
+++ IceTracker/Controllers/SightingsController.cs
@@ -18,10 +18,10 @@ namespace IceTracker.Controllers
         }
 
         [HttpPost("/sightings/{idUser}/save")]
-        public IActionResult CreateSighting(string description, string type, DateTime time, string address, string city, string state, string zip, int id)
+        public IActionResult CreateSighting(string description, string type, DateTime time, string address, string city, string state, string zip, int id, int idUser)
         {
-            Sighting newSighting = new Sighting(description, type, time, address, city, state, zip);
-            newSighting.Save();
+            Sighting newSighting = new Sighting(description, type, time, address, city, state, zip, idUser);
+            newSighting.Save(idUser);
             newSighting.Alert();
             return RedirectToAction("Index", "Home");          
         }
