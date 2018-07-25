320a6987ed16bbad823eea93937d282d49904ee0
Devin Mounts, Nikki Boyd and Ryan Putman <devin@>
2018-07-25 13:55:55 -0700

    add anonymous

--- IceTracker/wwwroot/js/site.js
+++ IceTracker/wwwroot/js/site.js
@@ -1,1 +1,13 @@
-﻿
+﻿   $( document ).ready(function() {
+        $("#anon").click(function() {
+            if (this.checked) {
+                $("#first-name").val("Anonymous");
+                $("#last-name").val("Anonymous");
+            }else if (!(this.checked)){
+                $("#first-name").val("");
+                $("#last-name").val("");
+            }
+
+        });
+
+    });
